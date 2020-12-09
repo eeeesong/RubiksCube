4236f6a9cd17ef3f0af72e7abb412324acd497ca
Song Lee <songlish233@gmail.com>
2020-12-09 15:33:18 +0900

    stepTwo.swift 생성, main의 step-1 내용 삭제

--- RubiksCube/main.swift
+++ RubiksCube/main.swift
@@ -1,31 +1,8 @@
 
 import Foundation
 
-let model = StepOne()
+let model = StepTwo()
 
-print(SystemMessage.info)
-
-let input = readLine() ?? ""
-
-let inputList = model.checkInput(for: input)
-
-guard !inputList.isEmpty else {
-    print(SystemMessage.inputError)
-    exit(0)
-}
-
-guard model.checkAction(for: inputList) else {
-    print(SystemMessage.actionError)
-    exit(1)
-}
-
-let word = inputList[0]
-let moveBy = inputList[1]
-let direction = inputList[2]
-
-let result = model.getResult(word: word, moveBy: Int(moveBy)!, direction: direction)
-
-print(SystemMessage.successMessage(word, moveBy, direction, result))
 
 
 
