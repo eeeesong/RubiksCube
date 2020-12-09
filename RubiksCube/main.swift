e10b6bd08f1bb8f40c59f669f4d3f19377100635
Song Lee <songlish233@gmail.com>
2020-12-09 15:47:53 +0900

    프로그램 시작 시 액션 안내 메시지와 시작 큐브 출력
    - SystemMessage 업데이트

--- RubiksCube/main.swift
+++ RubiksCube/main.swift
@@ -3,6 +3,10 @@ import Foundation
 
 let model = StepTwo()
 
+print(SystemMessage.info)
+print(SystemMessage.startingCube)
+let input = readLine() ?? ""
 
-
-
+//입력이 액션에 합치되는지 점검
+//잘못된 입력일 경우 프로그램 종료
+//올바른 입력일 경우 차례로 액션 실행
--- RubiksCube/systemMessage.swift
+++ RubiksCube/systemMessage.swift
@@ -2,18 +2,26 @@
 struct SystemMessage {
     
     static let info = """
-    📍변경할 문자(word)
-    📍움직일 정도(moveBy) * -100 이상 100 미만의 정수
-    📍방향(direction) * 오른쪽: R / 왼쪽: L
-    을 공백 포함하여 차례로 입력해주세요.
+    ⚡️R/R': 가장 오른쪽 줄 이동
+    ⚡️L/L': 가장 왼쪽 줄 이동
+    ⚡️U/U': 가장 윗줄 이동
+    ⚡️B/B': 가장 아랫 줄 이동
+    ⚡️Q: 프로그램 종료
+    """
+    
+    static let startingCube = """
+    \n현재 큐브:
+      R  R  W
+      G  C  W
+      G  B  B
     """
     
     static let inputError = "값을 모두 입력해주세요"
     static let actionError = "올바른 값을 입력해주세요"
     
-    static func successMessage(_ word: String, _ moveBy: String, _ direction: String,
-                               _ result: String) -> String {
-        return "👉🏻 \(word)가 \(moveBy)만큼 \(direction) 방향으로 움직입니다.\n결과: \(result)"
-    }
+//    static func successMessage(_ word: String, _ moveBy: String, _ direction: String,
+//                               _ result: String) -> String {
+//        return "👉🏻 \(word)가 \(moveBy)만큼 \(direction) 방향으로 움직입니다.\n결과: \(result)"
+//    }
     
 }
