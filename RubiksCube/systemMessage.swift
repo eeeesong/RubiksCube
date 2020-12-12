e2804902b1b7b85b441d9c5af971a3106045b925
Song Lee <songlish233@gmail.com>
2020-12-12 11:55:38 +0900

    info 메시지 수정, 구조체 명 SM으로 축약 변경

--- RubiksCube/systemMessage.swift
+++ RubiksCube/systemMessage.swift
@@ -1,11 +1,13 @@
 
-struct SystemMessage {
+struct SM {
     
     static let info = """
-    📍변경할 문자(word)
-    📍움직일 정도(moveBy) * -100 이상 100 미만의 정수
-    📍방향(direction) * 오른쪽: R / 왼쪽: L
-    을 공백 포함하여 차례로 입력해주세요.
+    1️⃣ 변경할 문자(word)
+    2️⃣ 움직일 정도(moveBy) * -100 이상 100 미만의 정수
+    3️⃣ 방향(direction) * 오른쪽: R / 왼쪽: L
+
+    1️⃣,2️⃣,3️⃣을 공백 포함하여 차례로 입력해주세요.
+
     """
     
     static let inputError = "값을 모두 입력해주세요"
